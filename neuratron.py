0832a57b7321012fa8b551b842af8e81c9412f5f
Pedrovinicius123 <pvasx123@gmail.com>
2025-04-12 09:28:13 -0300

    Backpropagation changes

--- neuratron.py
+++ neuratron.py
@@ -19,17 +19,18 @@ class Model:
         self.using_weight = self.total[:input_shape, :output_shape]
         self.using_bias = self.total_bias[:output_shape]
         self.output = np.dot(X, self.using_weight) + self.using_bias
-        
+
         return self.output
 
     def backward_pass(self, Y):
-        grad = 2 * np.mean(-self.output, axis=0)
+        grad = 2 * np.mean((Y-self.output).reshape(-1,1), axis=0)
+        self.using_bias += grad * self.lr  
+
         X_refited = np.array([np.sum(self.X, axis=0)])
         grad = np.dot(X_refited.T, grad)
-
         grad = np.clip(grad, -10, 10)
+        
         self.using_weight -= np.dot(self.using_weight.T, grad) * self.lr
-        self.using_bias = np.dot(self.using_bias, grad) * self.lr if len(grad.shape) > 1 else self.using_bias * grad * self.lr
 
         self.total[:self.using_weight.shape[0], :self.using_weight.shape[1]] = self.using_weight
         self.total_bias[:self.using_bias.shape[0]] = self.using_bias
--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_regression
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from alive_progress import alive_bar
 from neuratron import Model
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-X, y = make_regression(n_samples=300, random_state=98, n_features=3, noise=20)
+X, y = make_regression(n_samples=300, random_state=98, n_features=3, n_targets=1, noise=20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, shuffle=True, test_size=0.2)
 model = Model(shape=(20, 20), lr=0.001)
 epochs = 40000
@@ -17,12 +17,12 @@ with alive_bar(epochs) as bar:
     for i in range(epochs):
         output = model.feed_forward(X_train, 1)
         loss = mean_squared_error(output, y_train)
+        score = r2_score(output, y_train)
         model.backward_pass(y_train)
 
         if loss < 1000 and anterior_loss and anterior_loss < loss:
             break
 
         anterior_loss = loss
-
-        print(loss)
+        print(loss, score)
         bar()
